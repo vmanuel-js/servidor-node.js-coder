c3a4a4eea22899bb7e7454b2bcb3c953db1e17db
Victor Jordan <vmjordansolis@gmail.com>
2025-11-17 19:36:41 -0500

    Se convirtieron los archivos txt a json. También se terminaron los puntos de la entrega1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { error } from "console";
 import express from "express";
 import fs from "fs/promises";
 
@@ -46,10 +47,43 @@ class ProductosManager {
     await fs.writeFile(this.ruta, JSON.stringify(producto, null, 2));
     return nuevoProducto;
   }
+
+  async actualizarProducto(id, field) {
+    const productos = await this.obtenerProductos();
+    const index = productos.findIndex((p) => p.id === id);
+
+    if (index === -1) {
+      return null;
+    }
+
+    // Hacer que el ID no se actualice
+    const productoActualizado = {
+      ...productos[index],
+      ...field,
+      id: productos[index].id,
+    };
+
+    productos[index] = productoActualizado;
+
+    await fs.writeFile(this.ruta, JSON.stringify(productos, null, 2));
+    return productoActualizado;
+  }
+
+  async eliminarProducto(id) {
+    const productos = await this.obtenerProductos();
+    const index = productos.findIndex((p) => p.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    productos.splice(index, 1);
+    await fs.writeFile(this.ruta, JSON.stringify(productos, null, 2));
+    return true;
+  }
 }
 
 // Clase Carrito Manager
-
 class CarritoManager {
   constructor(ruta) {
     this.ruta = ruta;
@@ -78,13 +112,38 @@ class CarritoManager {
     await fs.writeFile(this.ruta, JSON.stringify(carrito, null, 2));
     return nuevoCarro;
   }
+
+  async obtenerCarritoPorId(id) {
+    const carritos = await this.obtenerCarrito();
+    return carritos.find((c) => c.id === id);
+  }
+
+  async agregarProductoAlCarrito(cid, pid) {
+    const carritos = await this.obtenerCarrito();
+    const carrito = carritos.find((c) => c.id === id);
+
+    if (!carrito) {
+      return null;
+    }
+
+    // Ver si el producto existe
+    const item = carrito.productos.find((p) => p.product == pid);
+
+    if (item) {
+      item.quantity += 1;
+    } else {
+      carrito.productos.push({ product: pid, quantity: 1 });
+    }
+
+    await fs.writeFile(this.ruta, JSON.stringify(carritos, null, 2));
+    return carrito;
+  }
 }
 
-const productManager = new ProductosManager("productos.txt");
-const cartManager = new CarritoManager("carrito.txt");
+const productManager = new ProductosManager("productos.json");
+const cartManager = new CarritoManager("carrito.json");
 
 // Rutas
-
 // GET -> /api/products
 servidor.get("/api/products", async (req, res) => {
   const productos = await productManager.obtenerProductos();
@@ -126,10 +185,64 @@ servidor.post("/api/products", async (req, res) => {
   res.json(nuevoProducto);
 });
 
+// PUT -> /api/products/:pid
+servidor.put("api/products/:pid", async (req, res) => {
+  const id = parseInt(req.params.pid);
+  const field = req.body;
+
+  const resultado = await productManager.actualizarProducto(id, field);
+  if (!resultado) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  res.json(resultado);
+});
+
+// DELETE /api/products/:pid
+servidor.delete("api/products/:pid", async (req, res) => {
+  const id = parseInt(req.params.pid);
+
+  const eliminado = await productManager.eliminarProducto(id);
+
+  if (!eliminado) {
+    return res.status(404).json({ error: "Producto no encontrado" });
+  }
+
+  res.json({ mensaje: "Producto eliminado correctamente" });
+});
+
 // POST -> /api/carts
 servidor.post("/api/carts", async (req, res) => {
   const nuevoCarrito = await cartManager.crearCarrito();
   res.json(nuevoCarrito);
+});
+
+// GET -> /api/carts/:cid
+servidor.get("/api/carts/:cid", async (req, res) => {
+  const cid = parseInt(req.params.cid);
+  const carrito = await cartManager.obtenerCarritoPorId(cid);
+
+  if (!carrito) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
+
+  res.json(carrito.productos);
+});
+
+// POST -> /api/carts/:cid/product/:pid
+servidor.get("/api/carts/:cid/product/:pid", async (req, res) => {
+  const cid = parseInt(req.params.cid);
+  const pid = parseInt(req.params.pid);
+  const carritoActualizado = await cartManager.agregarProductoAlCarrito(
+    cid,
+    pid
+  );
+
+  if (!carritoActualizado) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
+
+  res.json(carritoActualizado);
 });
 
 // Servidor en el puerto 8080
