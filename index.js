c4e218e173b999effc99d3e2e24e70ba5bcfc447
Victor Jordan <vmjordansolis@gmail.com>
2025-12-02 23:25:05 -0500

    Se instaló mongoose y validator, en el index se agregó el global error handling

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import productsViewRouter from "./routes/products.view.route.js";
 import cartsViewRouter from "./routes/carts.view.route.js";
 import realtimeRouter from "./routes/realtime.route.js";
 import ProductosManager from "./managers/ProductosManager.js";
+import mongoose from "mongoose";
 
 const PORT = 8080;
 const app = express();
@@ -22,13 +23,19 @@ servidorWS.on("connection", async (socket) => {
 
   socket.on("nuevoProducto", async (data) => {
     await ProductosManager.agregarProducto(data);
-    servidorWS.emit("listaProductos", await ProductosManager.obtenerProductos());
+    servidorWS.emit(
+      "listaProductos",
+      await ProductosManager.obtenerProductos()
+    );
   });
 
   socket.on("eliminarProducto", async (pid) => {
     await ProductosManager.eliminarProducto(pid);
-    servidorWS.emit("listaProductos", await ProductosManager.obtenerProductos());
-  })
+    servidorWS.emit(
+      "listaProductos",
+      await ProductosManager.obtenerProductos()
+    );
+  });
 });
 
 app.engine("handlebars", handlebars.engine());
@@ -46,7 +53,21 @@ app.use("/products", productsViewRouter);
 app.use("/carts", cartsViewRouter);
 app.use("/realtimeproducts", realtimeRouter);
 
-// Servidor en el puerto 8080
-servidor.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+// Global error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Error interno del servidor");
 });
+
+// Servidor en el puerto 8080
+mongoose
+  .connect("mongodb://127.0.0.1:27017/store-db")
+  .then(() => {
+    console.log("Se conectó a la BD");
+    servidor.listen(PORT, () => {
+      console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Hubo un error", error);
+  });
